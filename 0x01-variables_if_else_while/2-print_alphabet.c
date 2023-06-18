56be8f7c9ffef6246a44b14359f93ab1bac4fb7e
MookieMook24 <mathlakokekana24@gmail.com>
2023-06-18 14:01:59 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 /**
- *main - Entry Point
+ * main - Entry Point
  *
- *Return: Always 0 (Success)
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+	char i;
+
 	for (i = 'x'; i <= 'y' ; i++)
+	{
 		putchar(i);
+	}
 	putchar('\n');
+	return (0);
 }
