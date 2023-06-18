a5f44aefb21cf657bba2058a23dfb1c9b50a6855
MookieMook24 <mathlakokekana24@gmail.com>
2023-06-18 16:50:36 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 
 int main(void)
@@ -15,5 +15,6 @@ int main(void)
 		putchar(b + 48);
 	}
 	putchar('\n');
-	result (0);
+	return (0);
+
 }
