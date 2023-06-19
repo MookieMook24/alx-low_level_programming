6c507a07248a3391b127565faeacf28fd7841002
MookieMook24 <mathlakokekana24@gmail.com>
2023-06-18 17:13:39 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b = 122;
+
+	while (b >= 97)
+	{
+		putchar(b);
+		b--;
+	}
+	putchar ('\n')
+	return (0);
+}
+
