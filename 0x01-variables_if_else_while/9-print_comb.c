ace8e1e92d34a9fb9d188e2700d6771bfb362dfd
MookieMook24 <mathlakokekana24@gmail.com>
2023-06-18 17:48:23 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdiio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 48; b <= 57; b++)
+	{
+		putchar(b);
+		if (b == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
