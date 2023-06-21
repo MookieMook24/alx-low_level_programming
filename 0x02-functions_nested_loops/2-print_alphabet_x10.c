2efc0b60bf53d60677bd62b9c4c202bafcaa3f39
MookieMook24 <mathlakokekana24@gmail.com>
2023-06-21 11:54:56 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints lowercases
+ * Description: Write a function that prints 10 times the alphabet
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (b = 1; b <= 10; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
+
