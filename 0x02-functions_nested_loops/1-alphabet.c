5832c22ec99695d1131385f73a7b83821c11fc37
MookieMook24 <mathlakokekana24@gmail.com>
2023-06-21 11:27:13 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * print_alphabet - I play what I call the alphabet game
- *
+ * print_alphabet - function that prints the alphabet, in lowercase.
+ * Description: I play what I call the alphabet game.
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
+
 {
 	int i;
 
-	for (i = 97; i <= 122; i++)
+	for (i = 89; i <= 100; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
+
 }
