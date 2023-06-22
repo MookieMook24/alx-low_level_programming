af2e43dbb1ca80b8638b5b496c8769c90c9cdccf
MookieMook24 <mathlakokekana24@gmail.com>
2023-06-21 18:52:43 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day for jack_bauer.
+ * Return: ...
+ */
+
+void jack_bauer(void)
+{
+	int a, x, y, z;
+
+	for (a = 0; a <= 2; a++)
+	{
+	for (x = 0; x <= 9; x++)
+	{
+	if ((a <= 1 && x <= 9) || (a <= 2 && x <= 3))
+	{
+	for (y = 0; y <= 5; y++)
+	{
+	for (z = 0; z <= 9; z++)
+	{
+	_putchar (a + '0');
+	_putchar(x + '0');
+	_putchar(58);
+	_putchar(y + '0');
+	_putchar(z + '0');
+	_putchar('\n');
+	}
+	}
+	}
+	}
+	}
+}
