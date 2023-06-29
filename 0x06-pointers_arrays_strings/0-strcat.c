e525dec035895d67c2f39aa5a15c9fc3cba4e37e
MookieMook24 <mathlakokekana24@gmail.com>
2023-06-29 16:11:16 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _memset - fill a block of memory with a specific value
+ * @x: Starting address of memory to be filled
+ * @y: The desired value
+ * @z: Number of bytes to be changed
+ * Return: changed array with new value for n bytes
+ */
+
+char *_memset(char *x, char y, unsigned int z)
+
+{
+	int i = 0
+
+	for (; z > 0; i++)
+
+	{
+		x[i] = y;
+
+		z--;
+
+	}
+
+	return (x);
+
+}
